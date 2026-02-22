ee8285f94856da8e6d8f420e056e532716f77c10
bubu <1654388696@qq.com>
2026-02-22 13:55:13 +0800

    refactor(analytics-service): remove scanBasePackages override

--- analytics-service/src/main/java/com/nowcoder/community/analytics/AnalyticsServiceApplication.java
+++ analytics-service/src/main/java/com/nowcoder/community/analytics/AnalyticsServiceApplication.java
@@ -3,11 +3,10 @@ package com.nowcoder.community.analytics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.nowcoder.community")
+@SpringBootApplication
 public class AnalyticsServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnalyticsServiceApplication.class, args);
     }
 }
-

cc40e8e7237be334dd9fc79f7d96eea75f38e22c
bubu <1654388696@qq.com>
2026-02-22 14:04:14 +0800

    refactor(ops-service): remove scanBasePackages override

--- ops-service/src/main/java/com/nowcoder/community/ops/OpsServiceApplication.java
+++ ops-service/src/main/java/com/nowcoder/community/ops/OpsServiceApplication.java
@@ -3,11 +3,10 @@ package com.nowcoder.community.ops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.nowcoder.community")
+@SpringBootApplication
 public class OpsServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OpsServiceApplication.class, args);
     }
 }
-

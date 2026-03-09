0450b257773de79588eed10e59b26375b2e1cfdb
bubu <1654388696@qq.com>
2026-03-09 23:15:13 +0800

    chore(deploy): add im_core mysql init schema

--- deploy/mysql-init/001_create_databases.sh
+++ deploy/mysql-init/001_create_databases.sh
@@ -9,6 +9,10 @@ MYSQL_DATABASE="${MYSQL_DATABASE:-community}"
 MYSQL_USER="${MYSQL_USER:-}"
 MYSQL_PASSWORD="${MYSQL_PASSWORD:-}"
 
+IM_MYSQL_DATABASE="${IM_MYSQL_DATABASE:-im_core}"
+IM_MYSQL_USER="${IM_MYSQL_USER:-im_core}"
+IM_MYSQL_PASSWORD="${IM_MYSQL_PASSWORD:-imcorepass}"
+
 if [[ -z "${MYSQL_ROOT_PASSWORD}" ]]; then
   echo "[mysql-init] missing env: MYSQL_ROOT_PASSWORD" >&2
   exit 1
@@ -23,6 +27,13 @@ create database if not exists \`${MYSQL_DATABASE}\`
 
 create user if not exists '${MYSQL_USER:-community}'@'%' identified by '${MYSQL_PASSWORD:-communitypass}';
 grant select, insert, update, delete on \`${MYSQL_DATABASE}\`.* to '${MYSQL_USER:-community}'@'%';
+
+create database if not exists \`${IM_MYSQL_DATABASE}\`
+  default character set utf8mb4
+  default collate utf8mb4_unicode_ci;
+
+create user if not exists '${IM_MYSQL_USER}'@'%' identified by '${IM_MYSQL_PASSWORD}';
+grant select, insert, update, delete on \`${IM_MYSQL_DATABASE}\`.* to '${IM_MYSQL_USER}'@'%';
 
 flush privileges;
 SQL

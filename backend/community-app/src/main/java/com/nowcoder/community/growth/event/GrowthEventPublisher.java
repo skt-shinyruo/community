b49fe395dc4a372bae000322b08c901650d5c53b
bubu <1654388696@qq.com>
2026-04-23 17:21:04 +0800

    test: lock retired event delivery surface

--- backend/community-app/src/main/java/com/nowcoder/community/growth/event/GrowthEventPublisher.java
+++
@@ -1,8 +0,0 @@
-package com.nowcoder.community.growth.event;
-
-import com.nowcoder.community.growth.event.payload.CheckInPayload;
-
-public interface GrowthEventPublisher {
-
-    void publishCheckInCompleted(String eventId, CheckInPayload payload);
-}
